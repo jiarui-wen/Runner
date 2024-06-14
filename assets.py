8cec3f1d88bf9d3ebb1140e7abd4cdd96bf4aab6
jiarui-wen <wjr.wen@gmail.com>
2024-06-14 09:13:34 -0400

    Changed player
    much better :smirk:

--- assets.py
+++ assets.py
@@ -1,17 +1,27 @@
 import pygame
 from settings import *
+import os
 
 pygame.display.set_mode(RES)
 
 def load_img(name):
-    img = pygame.image.load('images/'+ name + '.png').convert_alpha()
+    img = pygame.image.load('images/'+ name).convert_alpha()
     # pygame.Surface.set_colorkey(img, colorkey)
-    # pygame.Surface.set_colorkey(img, img.get_at((1, 1)))
+    pygame.Surface.set_colorkey(img, img.get_at((1, 1)))
     return img
+
+def load_folder(folder_name):
+    images = []
+    for img in os.listdir('images/' + folder_name):
+        images.append(load_img(folder_name + '/' + img))
+    return images
+
 
 assets = {
     # 'torch': load_img('torch', (0, 0, 0)),
     # 'player': load_img('player', (48, 104, 80))
-    'torch': load_img('torch'),
-    'player': load_img('player')
+    'torch': load_img('torch.png'),
+    'player': {
+        'running': load_folder('player/running')
+    }
 }
--- spritesheets.py
+++ spritesheets.py
@@ -3,16 +3,15 @@ from assets import assets
 from settings import *
 
 class SpriteSheet:
-    def __init__(self, name, width, height, count, scale=1, speed=0.1, state='none'):
+    def __init__(self, name, width, height, scale=1, speed=0.1, state='none'):
         self.sheet = assets[name]
         self.width = width
         self.height = height
-        self.count = count
         self.scale = scale
         self.speed = speed
         self.pos = 0
         self.state = state
-        self.sprites = {}
+        self.sprites = assets[name]
 
     def set_state(self, state):
         self.state = state
@@ -20,29 +19,34 @@ class SpriteSheet:
 
     def animate(self):
         self.pos += self.speed
-        if self.pos >= self.count:
+        if self.pos >= len(self.sprites[self.state]):
             self.pos = 0
         return self.sprites[self.state][int(self.pos)]
     
 class PlayerSheet(SpriteSheet):
     def __init__(self):
-        super().__init__('player', 16, 16, 4, scale=8, state='forward')
-
-        self.sprites = {"forward": [],
-                        "left": [],
-                        "right": []}
+        super().__init__('player', 512, 512, speed=0.2, state='running')
         
-        for i in range(self.count): 
-            self.sprites['forward'].append(pygame.transform.scale_by(
-                self.sheet.subsurface(pygame.Rect((self.width * i, self.height * 4), (self.width, self.height))), self.scale))
-            self.sprites['left'].append(pygame.transform.scale_by(
-                self.sheet.subsurface(pygame.Rect((self.width * i, self.height * 3), (self.width, self.height))), self.scale))
-            self.sprites['right'].append(pygame.transform.scale_by(
-                self.sheet.subsurface(pygame.Rect((self.width * i, self.height * 5), (self.width, self.height))), self.scale))
+    
+# class PlayerSheet(SpriteSheet):
+#     def __init__(self):
+#         super().__init__('player', 16, 16, 4, scale=8, state='forward')
+
+#         self.sprites = {"forward": [],
+#                         "left": [],
+#                         "right": []}
+        
+#         for i in range(self.count): 
+#             self.sprites['forward'].append(pygame.transform.scale_by(
+#                 self.sheet.subsurface(pygame.Rect((self.width * i, self.height * 4), (self.width, self.height))), self.scale))
+#             self.sprites['left'].append(pygame.transform.scale_by(
+#                 self.sheet.subsurface(pygame.Rect((self.width * i, self.height * 3), (self.width, self.height))), self.scale))
+#             self.sprites['right'].append(pygame.transform.scale_by(
+#                 self.sheet.subsurface(pygame.Rect((self.width * i, self.height * 5), (self.width, self.height))), self.scale))
             
-        for state in self.sprites:
-            for img in self.sprites[state]:
-                pygame.Surface.set_colorkey(img, PLAYER_COLORKEY)
+#         for state in self.sprites:
+#             for img in self.sprites[state]:
+#                 pygame.Surface.set_colorkey(img, PLAYER_COLORKEY)
 
 
 
